85c9cdc9ef8a375dad3a798609c04c4148bcadc0
99cartiace-afk <99cartiace@gmail.com>
2026-02-05 16:56:57 +0530

    deployment changes
    pushed few deployment changes for github

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,31 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+// run on local
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+// run on server
+export default defineConfig({
+  base: "/velvet-dreams/",
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+})
